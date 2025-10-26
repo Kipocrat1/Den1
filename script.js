2cabb88370d46fdfb0811c5bcdd0496217176d17
Kipocrat1 <voilov.artem112@gmail.com>
2025-10-26 11:15:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -110,3 +110,11 @@ function setupAddClassButton(day) {
         }
     };
 }
+let block = document.querySelector('.block')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
