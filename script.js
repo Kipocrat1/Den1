4417dff80c71919996683e6dede86fc0610eac75
Kipocrat1 <voilov.artem112@gmail.com>
2025-10-26 11:24:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -116,5 +116,5 @@ document.addEventListener('mousemove', function(e) {
     let dy = e.pageY - window.innerHeight / 2
     let angleX = 60 * dx / window.innerWidth / 2
     let angleY = 60 * dy / window.innerHeight / 2
-    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+    block.style.transform = `rotateX(${angleY}deg) rotateY(${angleX}deg)`
 })
